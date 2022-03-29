3c1d223ac4c32c57d547c56ac6f63d0c68ba66d0
MarcelMoczarski <marcel.moczarski@tu-dortmund.de>
2022-03-29 05:31:03 +0000

    .

--- dl_training/training.py
+++ dl_training/training.py
@@ -1,4 +1,5 @@
-from dl_framework import data, callbacks, learner, model, loss_functions
+# from dl_framework import data, callbacks, learner, model, loss_functions
+import dl_framework as fw
 from pathlib import Path
 
 mnt_dir = Path("/content")
@@ -8,24 +9,24 @@ val_split = 0.2
 batch_size = 64
 num_classes = 10
 
-x_train, y_train, x_test, y_test = data.get_dataset("torchvision", "MNIST", mnt_dir)
-x_train, y_train, x_valid, y_valid = data.split_trainset(x_train, y_train, val_split)
+x_train, y_train, x_test, y_test = fw.data.get_dataset("torchvision", "MNIST", mnt_dir)
+x_train, y_train, x_valid, y_valid = fw.data.split_trainset(x_train, y_train, val_split)
 
-train_ds = data.Dataset(x_train, y_train)
-valid_ds = data.Dataset(x_valid, y_valid)
-test_ds = data.Dataset(x_test, y_test)
+train_ds = fw.data.Dataset(x_train, y_train)
+valid_ds = fw.data.Dataset(x_valid, y_valid)
+test_ds = fw.data.Dataset(x_test, y_test)
 
-train_dl, valid_dl, test_dl = data.get_dls(train_ds, valid_ds, test_ds, batch_size)
-train_db = data.DataBunch(train_dl, valid_dl, num_classes)
+train_dl, valid_dl, test_dl = fw.data.get_dls(train_ds, valid_ds, test_ds, batch_size)
+train_db = fw.data.DataBunch(train_dl, valid_dl, num_classes)
 
-monitor = callbacks.Monitor_Cb(["valid_acc", "valid_loss", "loss"])
-earlystopping = callbacks.EarlyStopping_Cb(monitor="valid_loss", patience=10)
+monitor = fw.callbacks.Monitor_Cb(["valid_acc", "valid_loss", "loss"])
+earlystopping = fw.callbacks.EarlyStopping_Cb(monitor="valid_loss", patience=2)
 
-callbackhandler = callbacks.CallbackHandler([monitor, earlystopping])
+callbacks = fw.callbacks.CallbackHandler([monitor, earlystopping])
 
-learn = learner.Learner(*model.get_model(train_db, model.Model_1), 
-                           loss_functions.cross_entropy, 
+learn = fw.learner.Learner(*fw.model.get_model(train_db, fw.model.Model_1), 
+                           fw.loss_functions.cross_entropy, 
                            train_db,
-                           callbackhandler
+                           callbacks
                            ) 
 learn.fit(20)
