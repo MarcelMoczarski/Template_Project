b1f54f61a2785824d8b25872aab46637bef8ea24
MarcelMoczarski <marcel.moczarski@tu-dortmund.de>
2022-04-04 11:52:53 +0000

    .

--- core/dl_framework/utils.py
+++ core/dl_framework/utils.py
@@ -1,10 +1,2 @@
-# from IPython.core.magic import register_cell_magic
-# from IPython import get_ipython
-# from mypy import api
-
-# @register_cell_magic
-# def mypy(line, cell):
-#   for output in api.run(['-c', '\n' + cell] + line.split()):
-#     if output and not output.startswith('Success'):
-#       raise TypeError(output)
-#   get_ipython().run_cell(cell)
+def read_config(config_file):
+    pass
