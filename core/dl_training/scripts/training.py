55b16ae9093ee563ab69b963e230b16408aa2960
MarcelMoczarski <marcel.moczarski@tu-dortmund.de>
2022-04-05 13:05:15 +0000

    .

--- core/dl_training/scripts/training.py
+++ core/dl_training/scripts/training.py
@@ -3,47 +3,49 @@ import click
 import toml
 from pathlib import Path
 import sys
-# val_split = 0.2
-# batch_size = 64
-# num_classes = 10
+val_split = 0.2
+batch_size = 64
+num_classes = 10
 
-# x_train, y_train, x_test, y_test = fw.data.get_dataset("torchvision", "MNIST", mnt_dir)
-# x_train, y_train, x_valid, y_valid = fw.data.split_trainset(x_train, y_train, val_split)
+mnt_dir = "/content"
 
-# train_ds = fw.data.Dataset(x_train, y_train)
-# valid_ds = fw.data.Dataset(x_valid, y_valid)
-# test_ds = fw.data.Dataset(x_test, y_test)
+x_train, y_train, x_test, y_test = fw.data.get_dataset("torchvision", "MNIST", mnt_dir)
+x_train, y_train, x_valid, y_valid = fw.data.split_trainset(x_train, y_train, val_split)
 
-# train_dl, valid_dl, test_dl = fw.data.get_dls(train_ds, valid_ds, test_ds, batch_size)
-# train_db = fw.data.DataBunch(train_dl, valid_dl, num_classes)
+train_ds = fw.data.Dataset(x_train, y_train)
+valid_ds = fw.data.Dataset(x_valid, y_valid)
+test_ds = fw.data.Dataset(x_test, y_test)
 
-# monitor = fw.callbacks.Monitor_Cb(["valid_acc", "valid_loss", "loss"])
-# earlystopping = fw.callbacks.EarlyStopping_Cb(monitor="valid_loss", patience=2)
+train_dl, valid_dl, test_dl = fw.data.get_dls(train_ds, valid_ds, test_ds, batch_size)
+train_db = fw.data.DataBunch(train_dl, valid_dl, num_classes)
 
-# callbacks = fw.callbacks.CallbackHandler([monitor, earlystopping])
+monitor = fw.callbacks.Monitor_Cb(["valid_acc", "valid_loss", "loss"])
+earlystopping = fw.callbacks.EarlyStopping_Cb(monitor="valid_loss", patience=1000)
 
-# learn = fw.learner.Learner(*fw.model.get_model(train_db, fw.model.Model_2), 
-#                            fw.loss_functions.cross_entropy, 
-#                            train_db,
-#                            callbacks
-#                            ) 
-# learn.fit(20)
+callbacks = fw.callbacks.CallbackHandler([monitor, earlystopping])
 
-@click.command()
-@click.argument("config_path", default="/content/Template_Project/configs", type=click.Path(exists=True))
+learn = fw.learner.Learner(*fw.model.get_model(train_db, fw.model.Model_1), 
+                           fw.loss_functions.cross_entropy, 
+                           train_db,
+                           callbacks
+                           ) 
+learn.fit(1000)
 
-def main(config_path):
+# @click.command()
+# @click.argument("config_path", default="/content/Template_Project/configs", type=click.Path(exists=True))
+
+# def main(config_path):
     
-    config_file = toml.load(Path(config_path)/"default_train_config.toml")
-    print(config_file)
-    # click.echo(sys.path)
-    with open(Path("./data/" + "test.txt"), "w") as file:
-        file.write("hi")
+#     config_file = toml.load(Path(config_path)/"default_train_config.toml")
+#     print(config_file)
+#     # click.echo(sys.path)
+#     with open(Path("./data/" + "test.txt"), "w") as file:
+#         file.write("hi")
 
-if __name__ == "__main__":
-    # config_path = "/config" 
-    # config_path = Path(config_path)
-    main()
+# if __name__ == "__main__":
+#     # config_path = "/config" 
+#     # config_path = Path(config_path)
+#     main()
 
 
 
